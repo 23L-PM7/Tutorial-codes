85b4710bbbf3f091c0f94f1b946e6de1229e75b7
erdenetsogta <a.erdenetsogt@gmail.com>
2024-04-09 17:03:32 +0800

    modal

--- login-flow/frontend/app/page.tsx
+++ login-flow/frontend/app/page.tsx
@@ -1,26 +1,44 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { fetcher } from "./util";
+import { ReactNode, useState } from "react";
 
 export default function Home() {
-  const [foods, setFoods] = useState([]);
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [signupOpen, setSignupOpen] = useState(false);
 
-  useEffect(() => {
-    //
-  }, []);
+  return (
+    <div>
+      <button type="button" className="btn" onClick={() => setLoginOpen(true)}>
+        Login
+      </button>
 
-  useEffect(() => {
-    fetcher("foods").then((data) => {
-      console.log(data);
-    });
-  }, []);
+      <button className="btn" onClick={() => setSignupOpen(true)}>
+        Sign Up
+      </button>
 
-  return null;
-  // if (loggedIn) {
-  //   return <MainApp />;
-  // }
+      <Modal open={loginOpen} onClose={() => setLoginOpen(false)}>
+        <h3 className="font-bold text-lg">Hello!1</h3>
+        <p className="py-4">LOGIN1</p>
+      </Modal>
 
-  // return <Login />;
+      <Modal open={signupOpen} onClose={() => setSignupOpen(false)}>
+        <p className="py-4">SIGN UP</p>
+      </Modal>
+    </div>
+  );
+}
+
+function Modal({ children, open, onClose }: { children: ReactNode; open: Boolean; onClose: () => void }) {
+  return (
+    <dialog className={`modal ${open ? "modal-open" : ""}`}>
+      <div className="modal-box">
+        {children}
+        <div className="modal-action">
+          <button className="btn" onClick={() => onClose()}>
+            Close
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
 }
