9f1832eb459331946b6144d9e8acbe0794a75933
erdenetsogta <a.erdenetsogt@gmail.com>
2024-04-22 17:17:42 +0800

    file upload

--- category-crud/backend/main.ts
+++ category-crud/backend/main.ts
@@ -2,13 +2,27 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import { Schema, connect, model } from "mongoose";
+import multer from "multer";
 import { nanoid } from "nanoid";
+// const cloudinary = require("cloudinary").v2;
 
-// type ICategory = {
-//   name: string;
-//   description: string;
-//   position: number;
-// };
+function getExtension(filename: string) {
+  const names = filename.split(".");
+  if (names.length > 1) {
+    return `.${names.pop()}`;
+  }
+  return "";
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "uploads/",
+    filename: function (req, file, cb) {
+      const filename = `${nanoid()}${getExtension(file.originalname)}`;
+      cb(null, filename);
+    },
+  }),
+});
 
 interface ICategory {
   _id: string;
@@ -32,56 +46,66 @@ const categorySchema = new Schema<ICategory>({
 
 const Category = model<ICategory>("Category", categorySchema);
 
-async function startServer() {
-  await connect(`${process.env.MONGODB_STRING}`);
+const app: Express = express();
+const port = process.env.PORT || 4000;
 
-  dotenv.config();
+dotenv.config();
 
-  const app: Express = express();
-  const port = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cors());
 
-  app.use(express.json());
-  app.use(cors());
+app.post("/upload", upload.single("file"), (req: Request, res: Response) => {
+  // req.file
 
-  // read many
-  app.get("/categories", async (req: Request, res: Response) => {
-    const categories = await Category.find({}, "_id name description").collation({ locale: "en" }).sort({ name: 1 });
-    res.json(categories);
+  console.log(req.file);
+
+  // cloudinary.v2.uploader.upload(req.file?.path, { upload_preset: "my_preset" }, (error: any, result: any) => {
+  //   console.log(result, error);
+  // });
+
+  res.json(req.file);
+});
+
+// read many
+app.get("/categories", async (req: Request, res: Response) => {
+  const categories = await Category.find({}, "_id name description").collation({ locale: "en" }).sort({ name: 1 });
+  res.json(categories);
+});
+
+// read one
+app.get("/categories/:id", async (req: Request, res: Response) => {
+  const one = await Category.findById(req.params.id);
+  res.json(one);
+});
+
+// create one
+app.post("/categories", async (req: Request, res: Response) => {
+  const form = req.body;
+  await Category.create({
+    _id: nanoid(),
+    ...form,
+    createdAt: Date.now(),
   });
+  res.sendStatus(201);
+});
 
-  // read one
-  app.get("/categories/:id", async (req: Request, res: Response) => {
-    const one = await Category.findById(req.params.id);
-    res.json(one);
-  });
+// update one
+app.put("/categories/:id", async (req: Request, res: Response) => {
+  const updates = req.body;
+  await Category.updateOne({ _id: req.params.id }, updates);
+  res.sendStatus(204);
+});
 
-  // create one
-  app.post("/categories", async (req: Request, res: Response) => {
-    const form = req.body;
-    await Category.create({
-      _id: nanoid(),
-      ...form,
-      createdAt: Date.now(),
-    });
-    res.sendStatus(201);
-  });
+// delete one
+app.delete("/categories/:id", async (req: Request, res: Response) => {
+  await Category.findByIdAndDelete(req.params.id);
+  res.sendStatus(204);
+});
 
-  // update one
-  app.put("/categories/:id", async (req: Request, res: Response) => {
-    const updates = req.body;
-    await Category.updateOne({ _id: req.params.id }, updates);
-    res.sendStatus(204);
-  });
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
 
-  // delete one
-  app.delete("/categories/:id", async (req: Request, res: Response) => {
-    await Category.findByIdAndDelete(req.params.id);
-    res.sendStatus(204);
-  });
-
-  app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-  });
-}
-
-startServer().catch((err) => console.error(err));
+connect(`${process.env.MONGODB_STRING}`).then(() => {
+  console.log("MongoDB started");
+});
--- category-crud/frontend/app/page.tsx
+++ category-crud/frontend/app/page.tsx
@@ -1,5 +1,32 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Home() {
-  return <div></div>;
+  const [uploading, setUploading] = useState(false);
+
+  async function handleUpload(e: any) {
+    setUploading(true);
+
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file, file.name);
+
+    const res = await fetch("http://localhost:4000/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      console.log({ data });
+      setUploading(false);
+    }
+  }
+
+  return (
+    <div>
+      <input type="file" name="file" disabled={uploading} onChange={handleUpload} />
+    </div>
+  );
 }
